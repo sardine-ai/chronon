c23ac817d3885d1dc793dca44f37fd8c5017a83d
Felipe <felipe.torres@sardine.ai>
2025-04-09 10:43:40 -0300

    set aggregator logs to debug level

--- aggregator/src/main/scala/ai/chronon/aggregator/windowing/SawtoothOnlineAggregator.scala
+++ aggregator/src/main/scala/ai/chronon/aggregator/windowing/SawtoothOnlineAggregator.scala
@@ -51,11 +51,13 @@ class SawtoothOnlineAggregator(val batchEndTs: Long,
 
   val batchTailTs: Array[Option[Long]] = tailTs(batchEndTs)
 
-  logger.info(s"Batch End: ${TsUtils.toStr(batchEndTs)}")
-  logger.info("Window Tails: ")
+  private var debugMsg = s"Batch End: ${TsUtils.toStr(batchEndTs)}"
+  debugMsg += "\nWindow Tails: "
   for (i <- windowMappings.indices) {
-    logger.info(s"  ${windowMappings(i).aggregationPart.outputColumnName} -> ${batchTailTs(i).map(TsUtils.toStr)}")
+    debugMsg += s"\n  ${windowMappings(i).aggregationPart.outputColumnName} -> ${batchTailTs(i).map(TsUtils.toStr)}"
   }
+
+  logger.debug(debugMsg)
 
   def update(batchIr: BatchIr, row: Row): BatchIr = update(batchEndTs, batchIr, row, batchTailTs)
 
